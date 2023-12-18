0878df7b1170acfa2d3d564c42a98f8df1c48351
AdriDevelopsThings <git@adridoesthings.com>
2023-12-19 00:12:50 +0100

    feat!: remove cli binary and merge server and cli together

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 mod bot;
 mod cleaning;
+mod cli;
 mod database;
 mod fetcher;
 mod filter;
@@ -8,5 +9,6 @@ mod user;
 
 pub use bot::{create_client, run_bot};
 pub use cleaning::start_cleaning;
+pub use cli::{reset_disruptions, show_users};
 pub use database::Database;
 pub use fetcher::start_fetching;
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,26 @@
 use std::env;
 
+use clap::Parser;
 use dotenv::dotenv;
 use env_logger::Env;
-use strecken_info_telegram::{run_bot, start_cleaning, start_fetching, Database};
+use strecken_info_telegram::{
+    reset_disruptions, run_bot, show_users, start_cleaning, start_fetching, Database,
+};
 use tokio::sync::mpsc;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    show_users: bool,
+    #[arg(short, long)]
+    reset_disruptions: bool,
+}
 
 #[tokio::main]
 async fn main() {
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    let args = Args::parse();
     dotenv().ok();
     let database = Database::new(
         &env::var("SQLITE_PATH").expect("No SQLITE_PATH environment variable supplied"),
@@ -15,10 +28,16 @@ async fn main() {
     .unwrap();
     database.initialize().unwrap();
 
-    let (telegram_message_sender, telegram_message_receiver) =
-        mpsc::unbounded_channel::<(i64, String)>();
+    if args.show_users {
+        show_users(database).await;
+    } else if args.reset_disruptions {
+        reset_disruptions(database).await;
+    } else {
+        let (telegram_message_sender, telegram_message_receiver) =
+            mpsc::unbounded_channel::<(i64, String)>();
 
-    start_fetching(database.clone(), telegram_message_sender);
-    start_cleaning(database.clone());
-    run_bot(database, telegram_message_receiver).await.unwrap();
+        start_fetching(database.clone(), telegram_message_sender);
+        start_cleaning(database.clone());
+        run_bot(database, telegram_message_receiver).await.unwrap();
+    }
 }
