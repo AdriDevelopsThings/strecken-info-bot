cde19aa6da0227e3c2c75fe604f709d0aafa566c
AdriDevelopsThings <git@adridoesthings.com>
2024-12-15 20:04:37 +0100

    fix: clippy

--- src/components/telegram/user.rs
+++ src/components/telegram/user.rs
@@ -40,22 +40,20 @@ impl User {
         }
 
         let mut filters_mapped = self.filters.iter().map(|filter| match filter {
-            Filter::Location { x, y, range } => {
-                return disruption
-                    .coordinates
-                    .iter()
-                    .map(|coordinate| {
-                        if !coordinate.x.is_normal() || !coordinate.y.is_normal() {
-                            return false;
-                        }
+            Filter::Location { x, y, range } => disruption
+                .coordinates
+                .iter()
+                .map(|coordinate| {
+                    if !coordinate.x.is_normal() || !coordinate.y.is_normal() {
+                        return false;
+                    }
 
-                        let (coordinate_x, coordinate_y) =
-                            epsg_3857_to_epsg_4326(coordinate.x, coordinate.y);
-                        let distance = epsg_4326_distance_km(coordinate_x, coordinate_y, *x, *y);
-                        distance <= (*range as f64)
-                    })
-                    .any(|x| x);
-            }
+                    let (coordinate_x, coordinate_y) =
+                        epsg_3857_to_epsg_4326(coordinate.x, coordinate.y);
+                    let distance = epsg_4326_distance_km(coordinate_x, coordinate_y, *x, *y);
+                    distance <= (*range as f64)
+                })
+                .any(|x| x),
             Filter::OnlyCancellations => disruption.track_restriction == TrackRestriction::Severe,
             Filter::RailwayManagement { letter } => disruption
                 .stations
