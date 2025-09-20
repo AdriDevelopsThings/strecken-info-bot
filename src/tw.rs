56fa222a709d52f217569cfa2df94dc2785dc081
Adrian <adrian@adrn.dev>
2025-09-20 19:24:59 +0200

    feat(mastodon): add tw regex

--- src/tw.rs
+++ src/tw.rs
@@ -1,11 +1,31 @@
-/// search for trigger warnings in a mesage
+use log::error;
+use regex::Regex;
+
+pub struct TriggerWarningRegex<'a> {
+    pub name: &'a str,
+    pub regex: &'a str,
+}
+
+impl<'a> TriggerWarningRegex<'a> {
+    pub fn new(name: &'a str, regex: &'a str) -> TriggerWarningRegex<'a> {
+        TriggerWarningRegex { name, regex }
+    }
+
+    fn get_regex(&self) -> Result<Regex, regex::Error> {
+        Regex::new(self.regex)
+    }
+}
+
+/// search for trigger warnings in a message
 /// if a triggering word was found the word gets returned
 /// if no word was found `None` gets returned
-pub fn get_message_tw_word(message: &str, trigger_warnings: &[&str]) -> Option<String> {
+/// strings in `trigger_warnings_words` must occur as own words (not as parts of other words)
+pub fn get_message_tw_word(message: &str, trigger_warning_words: &[&str]) -> Option<String> {
     if message.is_empty() {
         return None;
     }
 
+    // first we checn for triggering words
     let chars = message.chars().collect::<Vec<char>>();
     let mut words: Vec<String> = Vec::new();
     // split message into words
@@ -41,9 +61,36 @@ pub fn get_message_tw_word(message: &str, trigger_warnings: &[&str]) -> Option<S
         );
     }
 
-    for tw in trigger_warnings {
+    for tw in trigger_warning_words {
         if words.contains(&tw.to_string().to_lowercase()) {
             return Some(tw.to_string());
+        }
+    }
+
+    None
+}
+
+/// search for trigger warnings in a message
+/// if a regex applies on the message the name will be returned,
+/// otherwise None
+pub fn get_message_tw_regex(
+    message: &str,
+    trigger_warning_regex: &[TriggerWarningRegex],
+) -> Option<String> {
+    for regex in trigger_warning_regex {
+        let r = match regex.get_regex() {
+            Ok(r) => r,
+            Err(e) => {
+                error!(
+                    "Error while compiling regex name {} with regex {}: {e:?}",
+                    regex.name, regex.regex
+                );
+                continue;
+            }
+        };
+
+        if r.is_match(message) {
+            return Some(regex.name.to_string());
         }
     }
 
